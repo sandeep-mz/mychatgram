52874cbdd4a67da8bcb5f154f7bd85a7d9bfe64a
Sandeep Maurya <10millionvibration@gmail.com>
2024-10-06 18:17:22 +0530

    right side  06/10/2024

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import './RightSideBar.css'
-function RightSideBar() {
+import React from 'react';
+import './RightSideBar.css';
+import assets from '../../assets/assets'; 
+
+const RightSideBar = () => {
   return (
-    <div>
-      
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt='' />
+        <h3>Wisky Roam <img src={assets.green_dot} className='dot' alt="Online Status" /></h3>
+        <p>Hey, There I am Roam Wisky and I'm using this chat app.</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
     </div>
-  )
+  );
 }
 
-export default RightSideBar
+export default RightSideBar;
